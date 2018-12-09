161eface85381760818bf4a2cb8beb598e2398ef
Jenn Ah <jennah@Jenns-Air.home>
2018-12-08 19:13:53 -1000

    added dev script

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.EXPRESS_HOST_PORT || 8989;
 
-app.get('/smoke', (req, res) => {
+app.get('/api/smoke', (req, res) => {
     res.send('smoke test');
 })
 
 app.listen(PORT, () => {
     process.stdout.write(`Server listening on port: ${PORT}`);
-  });
+});
