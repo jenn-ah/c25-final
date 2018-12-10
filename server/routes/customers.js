f15b304d65d327685bf3ce2245e44a48e9448fe2
Jenn Ah <jennah@Jenns-MacBook-Air.local>
2018-12-09 17:12:45 -1000

    working on customers routes

--- server/routes/customers.js
+++ server/routes/customers.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const validator = require('validator');
+const Customer = require('../db/Models'); //fix Customer model when ready
 
 
-router.get('/smoke', (req, res) => {
-    res.send('smoke test for customers route');
+router.get('/', (req, res) => {
+  return Customer.fetchAll()
+    .then(customers => {
+      console.log('this is customers', customers);
+      return res.json(customers);
+    })
+    .catch(err => {
+      return res.status(500).json({ message: err.message, code: err.code });
+    });
 });
+
 
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.EXPRESS_HOST_PORT || 8989;
 
+const categoriesRouter = require('./routes/categories');
 const customersRouter = require('./routes/customers');
-const vendorsRouter = require('./routes/vendors');
 const postsRouter = require('./routes/posts');
+const vendorsRouter = require('./routes/vendors');
+
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.use('/api/categories', categoriesRouter);
 app.use('/api/customers', customersRouter);
-app.use('/api/vendors', vendorsRouter);
 app.use('/api/posts', postsRouter);
+app.use('/api/vendors', vendorsRouter);
 
 
 app.listen(PORT, () => {
