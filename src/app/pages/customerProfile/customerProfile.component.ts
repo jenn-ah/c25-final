278ba180322df7169a0d50399fca8611da50c5d7
Shadrach Lagrimas <shadrachlagrimas@Shadrachs-MBP.home>
2018-12-13 16:21:03 -1000

    profiles are functional

---
+++ src/app/pages/customerProfile/customerProfile.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from "../../services/backend.service";
+import { SessionService } from '../../services/session.service';
+import { Router } from '@angular/router';
+
+@Component({
+    templateUrl: './customerProfile.component.html',
+    styleUrls: ['./customerProfile.component.scss']
+})
+
+export class CustomerProfileComponent implements OnInit {
+    isLoggedIn: boolean = false;
+    loginPressed: boolean = false;
+    customer: object;
+
+    constructor(private backend: BackendService, private router: Router, private session: SessionService) {
+        this.customer = this.session.getCustomer();
+
+    }
+
+    ngOnInit() {
+    }
+
+    customerLogIn() {
+        return this.session.getIsLoggedIn();
+    }
+
+    logout() {
+        this.session.clearSession()
+        this.isLoggedIn = false;
+        this.loginPressed = false;
+        return this.router.navigate([''])
+    }
+}
