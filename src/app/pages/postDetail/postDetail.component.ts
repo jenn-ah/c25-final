38ad2e3fe6bffb1f254d9570764a5f57a81eddcf
Jennifer Hikichi <jenniferhikichi@Jennifers-MBP.home>
2018-12-15 17:03:53 -1000

    post detail made

---
+++ src/app/pages/postDetail/postDetail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './postDetail.component.html',
+  styleUrls: ['./postDetail.component.scss']
+})
+
+export class PostDetailComponent{
+  constructor(){}
+
+  editPost(){
+    
+  }
+}
