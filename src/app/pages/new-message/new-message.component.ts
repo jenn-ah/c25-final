e58a1a65761c552b83ed3a1501b4f85603d02a01
Shadrach Lagrimas <shadrachlagrimas@Shadrachs-MBP.home>
2018-12-19 09:57:07 -1000

    customer takes the username, hardcoded id, may need to adjust that later, but core functionality is there

--- src/app/pages/new-message/new-message.component.ts
+++ src/app/pages/new-message/new-message.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { MessageService } from '../../message.service';
+import { BackendService } from 'src/app/services/backend.service';
+import { SessionService } from 'src/app/services/session.service';
+
 
 @Component({
   selector: 'app-new-message',
@@ -7,10 +10,19 @@ import { MessageService } from '../../message.service';
   styleUrls: ['./new-message.component.scss']
 })
 export class NewMessageComponent {
-  user: string;
+  user: any;
   message: string;
+  data: any;
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private backend: BackendService, private session: SessionService) { 
+          this.backend.getCustomer()
+          .then((data) => {
+            this.user = data
+            console.log(this.user)
+          })
+          .catch((err)=>{console.log(err)})
+
+  }
 
   newMessage(text: string, user: string): void {
     this.messageService.send({text: text, user: user});
