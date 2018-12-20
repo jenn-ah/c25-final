d924a715120efd026308c7598c100ae215dd39a7
Shadrach Lagrimas <shadrachlagrimas@Shadrachs-MBP.home>
2018-12-19 16:33:32 -1000

    fix pr issues

--- src/app/pages/postDetail/postDetail.component.ts
+++ src/app/pages/postDetail/postDetail.component.ts
@@ -8,8 +8,40 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 })
 
 export class PostDetailComponent implements OnInit {
-  post: object;
-  
+  post: {
+    id: number,
+    title: string,
+    username: string,
+    description: string,
+    photo: string,
+    post_status_id: number,
+    post_priority_id: number,
+    city: string,
+    state: string,
+    zip_code: string,
+    email: string,
+    customer_id: number,
+    customerId: string,
+    can_bid: boolean,
+    budget: number
+  } = {
+      id: -1,
+      title: '',
+      description: '',
+      username: '',
+      photo: '',
+      post_status_id: null,
+      post_priority_id: null,
+      city: '',
+      state: '',
+      zip_code: '',
+      email: '',
+      customer_id: null,
+      customerId: null,
+      can_bid: false,
+      budget: null
+    }
+
   constructor(
     private backend: BackendService,
     private router: Router,
